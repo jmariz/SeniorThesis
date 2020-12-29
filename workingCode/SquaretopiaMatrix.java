f959ea5b10b278c311efe276431a1148fd13dc59
jmariz <30633499+jmariz@users.noreply.github.com>
2020-12-29 01:40:03 -0800

    Update SquaretopiaMatrix.java

--- workingCode/SquaretopiaMatrix.java
+++ workingCode/SquaretopiaMatrix.java
@@ -3,20 +3,23 @@ package partition;
 import java.util.HashSet;
 import java.util.Set;
 
-/******************************************************************************
- *  Compilation:  javac Matrix.java
- *  Execution:    java Matrix
- *
- *  A bare-bones immutable data type for M-by-N matrices.
- *
- ******************************************************************************/
-
+/**
+ * Joshua Mariz
+ * Squaretopia representation using an array of SquaretopiaState arrays
+ */
 final public class SquaretopiaMatrix {
+    
     private final int M;             // number of rows
     private final int N;             // number of columns
-    public final SquaretopiaState[][] data;   // M-by-N array
-
-    // create M-by-N matrix of 0's
+    public final SquaretopiaState[][] data;   // M-by-N SquaretopiaState array
+    
+    /**
+     * Sets up an M by N representation of Squaretopia using an array of SquaretopiaState arrays.
+     * <b>NOTE: This method initializes the outer padding layer of the Squaretopia matrix to -1. This is so that each inner state has four adjacent neighbors.</b>
+     * @param M Integer number of rows in this Squaretopia
+     * @param N Integer number of columns in this Squaretopia
+     * @return void
+     */
     public SquaretopiaMatrix(int M, int N) {
         this.M = M;
         this.N = N;
@@ -32,10 +35,14 @@ final public class SquaretopiaMatrix {
         }
     }
     
-    // print matrix to standard output
+    /**
+     * Prints a String representation of this Squaretopia. Prints a matrix where each number corresponds to the district number of that Squaretopia state. 
+     * <b>NOTE: This method does not print the outer padding layer of -1s.</b>
+     * @return void
+     */
     public void show() {
-        for (int i = 1; i < M - 1; i++) { // exclude perimeter of -1s
-            for (int j = 1; j < N - 1; j++) {
+        for (int i = 1; i < M - 1; i++) { // exclude outer rows of -1s
+            for (int j = 1; j < N - 1; j++) { // exclude outer columns of -1s
                 int districtNum = data[i][j].districtNumber;
                 if(districtNum >= 0 && districtNum <= 9) {
                     System.out.print("   " + data[i][j].districtNumber);
@@ -49,7 +56,11 @@ final public class SquaretopiaMatrix {
         }
     }
     
-    public void checkers() {
+    /**
+     * Helpful method for debugging. Prints a matrix containing the SquaretopiaState.checked values for each state in this Squaretopia.
+     * @return void
+     */
+    public void printCheckedValues() {
         for (int i = 1; i < M - 1; i++) {
             for (int j = 1; j < N - 1; j++) { 
                 System.out.print(" " + data[i][j].checked);
@@ -59,33 +70,21 @@ final public class SquaretopiaMatrix {
         }
     }
     
-    // creates a duplicate of our matrix so we can change stuff
-    public static SquaretopiaMatrix duplicateMatrix(SquaretopiaMatrix matrix) {
-        int matrixLength = matrix.data.length; // assumes matrix is a square
-        SquaretopiaMatrix matrixDuplicate = new SquaretopiaMatrix(matrixLength, matrixLength);
-        for(int i = 0; i < matrixLength; i++) {
-            for(int j = 0; j < matrixLength; j++) {
-                matrixDuplicate.data[i][j].checked = matrix.data[i][j].checked; // only care about unchecked states
-            }
-        }
-        return matrixDuplicate;
-    }
-    
-    // determines if this mapping is valid by propagating through neighbors and determining isolated groups of less than n states 
-    public static Boolean validMap(SquaretopiaMatrix matrix) {
-        int matrixLength = matrix.data.length;
-        SquaretopiaMatrix matrixCopy = duplicateMatrix(matrix);
+    /**
+     * Determines if this Squaretopia matrix is valid. An invalid matrix contains at least one isolated group of k Squaretopia states, 
+     * where n, the number of states in each district, doesn't divide k.
+     * <b>NOTE: This method assumes that this Squaretopia matrix is a square.</b>
+     * @param matrix SquaretopiaMatrix whose validity we will check
+     * @return Boolean value for the statement: This matrix is valid.
+     */ 
+    public Boolean validMap() {
+        int matrixLength = data.length;
+        SquaretopiaMatrix matrixCopy = duplicateMatrix();
         for(int i = 1; i < matrixLength - 1; i++) {
             for (int j = 1; j < matrixLength - 1; j++) {
                 if (matrixCopy.data[i][j].checked == false) {
-//                    System.out.println("checking: (" + i + " , " + j + ")" ); // delete
-                    Set<SquaretopiaState> neighborStates = findContiguousNeighbors(matrixCopy, matrixCopy.data[i][j]);
-                    // System.out.println("contiguousStates.size(): " + findContiguousNeighbors(matrixCopy, matrixCopy.data[i][j]).size()); // delete
-//                    for(SquaretopiaState state : neighborStates) {
-//                        System.out.println(state.toString()); // delete
-//                    }
-                    // System.out.println("crap: " + findContiguousNeighbors(matrixCopy, matrixCopy.data[i][j]).size()); // delete
-//                    System.out.println("neighbors.size(): " + neighborStates.size()); // delete
+                    // the set below contains all the unchecked neighbors that a rook chess piece can reach when starting from the i, j location
+                    Set<SquaretopiaState> neighborStates = findContiguousNeighbors(matrixCopy, matrixCopy.data[i][j]);  
                         if(neighborStates.size() % (matrixLength - 2) !=  0) {
                             return false;
                         }
@@ -95,9 +94,31 @@ final public class SquaretopiaMatrix {
         return true;
     }
     
-    // adds a specified state's contiguous neighbors to a set 
+    /**
+     * Creates a SquaretopiaMatrix duplicate of this Squaretopia matrix.
+     * <b>NOTE: This method assumes that this Squaretopia matrix is a square.</b>
+     * @param matrix SquaretopiaMatrix that we will duplicate
+     * @return void
+     */
+    public SquaretopiaMatrix duplicateMatrix() {
+        int matrixLength = data.length; // assumes matrix is a square
+        SquaretopiaMatrix matrixDuplicate = new SquaretopiaMatrix(matrixLength, matrixLength);
+        for(int i = 0; i < matrixLength; i++) {
+            for(int j = 0; j < matrixLength; j++) {
+                // we only need the checked field to determine if a completed district is valid
+                matrixDuplicate.data[i][j].checked = data[i][j].checked;
+            }
+        }
+        return matrixDuplicate;
+    }
+    
+    /**
+     * Creates a set containing all the unchecked neighbors that a rook chess piece can reach when starting from a given location in this Squaretopia.
+     * @param matrix SquaretopiaMatrix that contains Squaretopia states and some district assignments
+     * @param currentLocation SquaretopiaState whose contiguous unchecked neighbors we will find
+     * @return Set<SquaretopiaState> containing all the unchecked neighbors that a rook chess piece can reach when starting from currentLocation
+     */
     public static Set<SquaretopiaState> findContiguousNeighbors (SquaretopiaMatrix matrix, SquaretopiaState currentLocation) {
-//        System.out.println("passed state " + currentLocation.toString()); // delete
         int curLocRow = currentLocation.row;
         int curLocCol = currentLocation.col;
         Set<SquaretopiaState> contiguousStates = new HashSet<>();
@@ -115,68 +136,7 @@ final public class SquaretopiaMatrix {
         if(matrix.data[curLocRow][curLocCol + 1].checked == false) { // check availability of the state to the right
             contiguousStates.addAll(findContiguousNeighbors(matrix, matrix.data[curLocRow][curLocCol + 1]));
         }
-//        System.out.println("from check method contiguousstates.size(): " + contiguousStates.size()); // delete
         return contiguousStates;
     }
 
-//    // create matrix based on 2d array
-//    public Matrix(int[][] data) {
-//        M = data.length;
-//        N = data[0].length;
-//        this.data = new int[M][N];
-//        for (int i = 0; i < M; i++) {
-//            for (int j = 0; j < N; j++) {
-//                if(i == 0 || i == M - 1 || j == 0 || j == N - 1) {
-//                    this.data[i][j] = -1;
-//                } else {
-//                    this.data[i][j] = data[i][j];
-//                }
-//            }
-//        }
-//    }
-
-//    // copy constructor
-//    private Matrix(Matrix A) { this(A.data); }
-
-//    // create and return a random M-by-N matrix with values between 0 and 1
-//    public static Matrix random(int M, int N) {
-//        Matrix A = new Matrix(M, N);
-//        for (int i = 0; i < M; i++)
-//            for (int j = 0; j < N; j++)
-//                A.data[i][j] = Math.random();
-//        return A;
-//    }
-
-//    // create and return the N-by-N identity matrix
-//    public static Matrix identity(int N) {
-//        Matrix I = new Matrix(N, N);
-//        for (int i = 0; i < N; i++)
-//            I.data[i][i] = 1;
-//        return I;
-//    }
-//
-//    // swap rows i and j
-//    private void swap(int i, int j) {
-//        int[] temp = data[i];
-//        data[i] = data[j];
-//        data[j] = temp;
-//    }
-
-//    // return C = A + B
-//    public Matrix plus(Matrix B) {
-//        Matrix A = this;
-//        if (B.M != A.M || B.N != A.N) throw new RuntimeException("Illegal matrix dimensions.");
-//        Matrix C = new Matrix(M, N);
-//        for (int i = 0; i < M; i++)
-//            for (int j = 0; j < N; j++)
-//                C.data[i][j] = A.data[i][j] + B.data[i][j];
-//        return C;
-//    }
-
-
-
 }
-
-
-// Copyright © 2000–2017, Robert Sedgewick and Kevin Wayne.
-// Last updated: Fri Oct 20 14:12:12 EDT 2017.
